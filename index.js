a54457278024aef1698514ccd5bc0d5095c1e305
Kah Yap <kahyap13@gmail.com>
2018-10-30 21:05:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, newName) {
-  katzDeliLine.push(`${newName}`);
-  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
+let katzDeliLine = [] // create empty array
+
+let takeANumber = (katzDeliLine, newName) => { // push name and line number to array
+  katzDeliLine.push(newName);
+  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line`
 }
 
-<<<<<<< HEAD
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
+let nowServing = katzDeliLine => { // lets you know whos turn it is and shifts array
+  if(katzDeliLine.length) {
     return `Currently serving ${katzDeliLine.shift()}.`
   }
   return "There is nobody waiting to be served!"
 }
 
-function currentLine(katzDeliLine) {
-  var line = []
-  if(katzDeliLine.length === 0) {
-    return "The line is currently empty."
-  } else {
-    for(var i = 0; i < katzDeliLine.length; i++) {
-      line.push(` ${i + 1}. ${katzDeliLine[i]}`);
+let currentLine = katzDeliLine => { // lets you know who is on the line 
+  let array = []
+  if(katzDeliLine.length) {
+    for(let i = 0; i < katzDeliLine.length; i++) {
+      array.push(` ${i + 1}. ${katzDeliLine[i]}`)
     }
-    return `The line is currently:${line}`
-=======
-function nowServing() {
-  if {
-    return "Currently serving ${katzDeliLine[0]}"
->>>>>>> 64986653944eeed4b29cc261c148db84b3f2be6e
+    return `The line is currently: ${array}`
   }
 }
+
+takeANumber(katzDeliLine, "Kah")
+takeANumber(katzDeliLine, "Ludy")
+nowServing(katzDeliLine)
+currentLine(katzDeliLine)
+
